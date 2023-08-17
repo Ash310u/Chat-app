3a8986c456ac6782d3cd82eef0d35aab140afb75
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-17 09:40:58 +0530

    Requiring socketio and called with the raw http server, Now my server supports web sockets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 const path = require('path')
 const http = require('http')
 const express = require('express')
+const socketio = require('socket.io')
 
 const app = express()
 // Now if I don't do this express library does this behind the scenes anyways. I'm not changing the behavior. I'm just doing a little bit of refactoring.
 const server = http.createServer(app)
+// socketIo expects it to called with the raw http server (I have that in the server veriable). Now express creates that behind the scenes I don't have access to it to pass it, That's why I created it on my own.
+const io = socketio(server)
 
 const port = process.env.PORT || 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
