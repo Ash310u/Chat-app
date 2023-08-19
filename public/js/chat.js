32637a1e7f41df5ab0fd328bd476665fe40003da
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-19 16:31:51 +0530

    Commited Elements, set a disable and enble function for send button and add some UI features like clearing the input also focus back on it

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,18 +1,37 @@
 const socket = io()
 
+// Elements
+
+const $messageForm = document.querySelector('#msg-form')
+const $messageFormInput = $messageForm.querySelector('input')   
+const $messageFormButton = $messageForm.querySelector('button')   
+
 // receiving the event that the server is sending to client.
 socket.on('message', (msg) => {
     console.log(msg);
 })
 
-document.querySelector('#msg-form').addEventListener('submit', (e) => {
+$messageForm.addEventListener('submit', (e) => {
     e.preventDefault()
+
+    // Disabled until message is sent, for next msg to be send
+    $messageFormButton.setAttribute('disabled', 'disabled')
 
     // Target represents the target that I'm listening for the event on and in this case that's form('#msg-form')
     // (e.target.elements.message) = that "message" input.
-    const MSG = e.target.elements.message.value
+    const MSG = $messageFormInput.value
                                 // Last argument on emit a callback function for acknowledgement.
     socket.emit('sendMessage', MSG, (error) => {
+
+        // Waiting for message to be sent
+        $messageFormButton.removeAttribute('disabled')
+        
+        // Clearing input after sending message
+        $messageFormInput.value = ''
+
+        // focus back the input
+        $messageFormInput.focus()
+
         if (error) {
             return console.log(error);            
         }
