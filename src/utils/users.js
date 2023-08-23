09ad20592e7f7b83cc0e91aee2aa46186b43ba15
Ash310u <107404699+Ash310u@users.noreply.github.com>
2023-08-23 13:09:19 +0530

    Commited exporting the functions

--- src/utils/users.js
+++ src/utils/users.js
@@ -52,3 +52,10 @@ const getUserInRoom = (room) => {
     room = room.trim().toLowerCase()
     return users.filter(user => user.room === room)
 }
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUserInRoom
+}
